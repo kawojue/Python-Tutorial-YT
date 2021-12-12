f67ccc237044d3d0ab8a3d14afc8afc1b0feb6d1
Kawojue Raheem Olumuyiwa <96027920+alwaysappear@users.noreply.github.com>
2021-12-13 00:46:13 +0100

    Add files via upload

---
+++ What are variables/data types and variable.py
@@ -0,0 +1,60 @@
+a = 5
+
+print(a)
+
+print(type(a))
+
+name = "Bob"
+
+print(type(name))
+print(name)
+
+b = "6"
+b = int(b)
+print(b)
+
+print(type(b))
+
+c = 5.5
+print(c)
+
+print(type(c))
+
+a = b = 7
+
+print(a, b)
+
+_f = "Bob"
+
+print(_f)
+
+F = 6
+print(F)
+
+age = 19
+print(age)
+
+Age = 17
+print(Age)
+
+_Age = 18
+print(_Age)
+
+AGE = "Sixteen"
+print(AGE)
+
+false = False
+
+true = True
+
+print(false, true)
+
+comp = complex(3, 5)
+
+print(comp.conjugate())
+
+print(comp.imag)
+
+print(comp.real)
+
+
